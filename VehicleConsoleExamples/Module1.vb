7d54a523ec60da13ad65086e9e06c3a791d34a2a
Karen Payne <paynekaren@comcast.net>
2019-12-14 16:38:55 -0800

    Added vehicles mocked examples

---
+++ VehicleConsoleExamples/Module1.vb
@@ -0,0 +1,23 @@
+﻿Imports VehicleLibrary
+
+Module Module1
+
+    Sub Main()
+        Dim standardColor = Console.ForegroundColor
+        Dim operations = New VehicleExamples
+
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Group By Manufacturer Then By Year")
+        Console.ForegroundColor = standardColor
+
+
+        operations.GroupByManufacturerThenByYear()
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("Group By Manufacturer Then By Year")
+        Console.ForegroundColor = standardColor
+        operations.GroupByManufacturerOrderByManufacturer()
+
+        Console.ReadLine()
+    End Sub
+
+End Module
