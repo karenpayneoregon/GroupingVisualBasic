d4a6f5b54b1100d632e3c874a412c345c8eb7b59
Karen Payne <paynekaren@comcast.net>
2020-01-01 09:44:10 -0800

    Split up an example

---
+++ NorthWindAzureGroupCustomerByCountry/Classes/CountryCompanyContainer.vb
@@ -0,0 +1,10 @@
+﻿Imports NorthWindAzureSqlClient
+
+Namespace Classes
+
+    Friend Class CountryCompanyContainer
+        Public Property CountryName As String
+        Public Property Customers As IEnumerable(Of Customer)
+        Public Property Count As Integer
+    End Class
+End Namespace
--- NorthWindAzureGroupCustomerByCountry/Module1.vb
+++ NorthWindAzureGroupCustomerByCountry/Module1.vb
@@ -1,4 +1,5 @@
-﻿Imports NorthWindAzureSqlClient
+﻿Imports NorthWindAzureGroupCustomerByCountry.Classes
+Imports NorthWindAzureSqlClient
 ''' <summary>
 ''' Code sample taken from
 ''' https://docs.microsoft.com/en-us/dotnet/visual-basic/language-reference/queries/group-by-clause
@@ -8,19 +9,25 @@
 ''' Some name changes were done.
 ''' </summary>
 Module Module1
+
+    Sub Main()
+
+
+        Console.ReadLine()
+
+    End Sub
     ''' <summary>
     ''' The following code example groups a list of customers based on their location 
     ''' (country/region) and provides a count of the customers in each group. The results are ordered 
     ''' by country/region name. The grouped results are ordered by city name.
     ''' </summary>
-    Sub Main()
+    Private Sub AnonymousVersion()
         Dim operations = New DataOperations
         Dim customers = operations.CustomerList()
 
         Dim customersByCountry = From customer In customers
                                  Order By customer.City
-                                 Group By CountryName = customer.Country
-                                 Into regionalCustomers = Group, Count()
+                                 Group By CountryName = customer.Country Into regionalCustomers = Group, Count()
                                  Order By CountryName
 
         For Each country In customersByCountry
@@ -30,11 +37,67 @@ Module Module1
             For Each customer In country.regionalCustomers
                 Console.WriteLine($"{customer.CustomerIdentifier,5} {customer.CompanyName} ({customer.City})")
             Next
+
+
             Console.WriteLine()
+
         Next
 
-        Console.ReadLine()
+    End Sub
+    Sub AnonymousToStrongTypedVersion()
+        Dim operations = New DataOperations
+        Dim customers = operations.CustomerList()
+
+        Dim baseQuery = From customer In customers Order By customer.City
+                        Group By CountryName = customer.Country Into regionalCustomers = Group Order By CountryName
+
+        Dim customersByCountry =
+                baseQuery.Select(Function(customer) New CountryCompanyContainer With {
+                                    .CountryName = customer.CountryName,
+                                    .Customers = customer.regionalCustomers,
+                                    .Count = customer.regionalCustomers.Count()
+                                    })
+
+        For Each TopGroup In customersByCountry
+            Console.WriteLine($"{TopGroup.CountryName} ({TopGroup.Count})")
+            For Each innerGroup In TopGroup.Customers
+                Console.WriteLine($"{innerGroup.CustomerIdentifier,5} {innerGroup.CompanyName} ({innerGroup.City})")
+            Next
+        Next
+
 
     End Sub
+    ''' <summary>
+    ''' Given code from AnonymousToStrongTypedVersion procedure
+    ''' above the base query is now simply the starting point
+    ''' </summary>
+    Sub AnonymousToStrongTypedVersionToMuch()
+        Dim operations = New DataOperations
+        Dim customers = operations.CustomerList()
 
+
+        Dim customersByCountry =
+                (
+                    From customer In customers
+                    Order By customer.City
+                    Group By CountryName = customer.Country Into regionalCustomers = Group
+                    Order By CountryName).Select(Function(customer) New CountryCompanyContainer With {
+                                                    .CountryName = customer.CountryName,
+                                                    .Customers = customer.regionalCustomers,
+                                                    .Count = customer.regionalCustomers.Count()
+                    }
+                )
+
+        For Each TopGroup In customersByCountry
+
+            Console.WriteLine($"{TopGroup.CountryName} ({TopGroup.Count})")
+
+            For Each innerGroup In TopGroup.Customers
+                Console.WriteLine($"{innerGroup.CustomerIdentifier,5} {innerGroup.CompanyName} ({innerGroup.City})")
+            Next
+
+        Next
+
+
+    End Sub
 End Module
